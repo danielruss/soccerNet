fe182fa11d87d8f4989e85bccf963ce37d883727
Daniel Russ <druss@mail.nih.gov>
2024-12-11 13:09:43 -0500

    no need for subdirectory

---
+++ clips.mjs
@@ -0,0 +1,11 @@
+export class clips {
+    static version_info = new Map({
+        "0.0.1": {
+            "url": "https://danielruss.github.io/soccerNet/clips_0.0.1.onnx",
+            "embedding_model_name": 'Xenova/GIST-small-Embedding-v0',
+            "version": "3.0.5",
+            "pooling": "cls",
+            "train_data": "industry_training_data1.json"
+        }
+    })
+}
